f4f741df7990b87cfa7fd15cf64d4253438c15af
MHRafix <fmhrafiz27@gmail.com>
2024-11-13 16:44:44 +0600

    fixed image

--- src/app/config/logic/fileUploader.ts
+++ src/app/config/logic/fileUploader.ts
@@ -1,24 +1,34 @@
-export function fileUploader(userpic: File, preset: string) {
-	// avatar uploaded to cloudinary cloud service
-	const file_upload_cloudinary = async () => {
-		if (userpic) {
-			const data = new FormData();
-			data.append('file', userpic);
-			data.append('upload_preset', preset);
-			data.append('cloud_name', 'CoderXone');
-			const upload_req = await fetch(
-				'https://api.cloudinary.com/v1_1/CoderXone/image/upload',
-				{
-					method: 'POST',
-					body: data,
-				}
-			);
-			const file_uploaded = await upload_req.json();
-			return file_uploaded.url;
-		} else {
-			return;
-		}
-	};
+import { showNotification } from "@mantine/notifications";
 
-	return { file_upload_cloudinary };
+export function fileUploader(userpic: File, preset: string) {
+  // avatar uploaded to cloudinary cloud service
+  const file_upload_cloudinary = async () => {
+    if (userpic?.size > 3145728) {
+      showNotification({
+        title: "File size is too large",
+        message: "File size must be lower than 3 mb",
+        color: "red",
+      });
+    } else {
+      if (userpic) {
+        const data = new FormData();
+        data.append("file", userpic);
+        data.append("upload_preset", preset);
+        data.append("cloud_name", "CoderXone");
+        const upload_req = await fetch(
+          "https://api.cloudinary.com/v1_1/CoderXone/image/upload",
+          {
+            method: "POST",
+            body: data,
+          }
+        );
+        const file_uploaded = await upload_req.json();
+        return file_uploaded.url;
+      } else {
+        return;
+      }
+    }
+  };
+
+  return { file_upload_cloudinary };
 }
--- src/components/custom/TravelPackage/CreatePackage/UploadThumbnails.tsx
+++ src/components/custom/TravelPackage/CreatePackage/UploadThumbnails.tsx
@@ -1,326 +1,326 @@
-import { Notify } from '@/app/config/alertNotification/Notification';
+import { Notify } from "@/app/config/alertNotification/Notification";
 import {
-	CREATE_TRAVEL_PACKAGE,
-	Update_Tour_Package,
-} from '@/app/config/gql-queries/travelPackage.query';
-import { fileUploader } from '@/app/config/logic/fileUploader';
-import { useGetSession } from '@/app/config/logic/getSession';
+  CREATE_TRAVEL_PACKAGE,
+  Update_Tour_Package,
+} from "@/app/config/gql-queries/travelPackage.query";
+import { fileUploader } from "@/app/config/logic/fileUploader";
+import { useGetSession } from "@/app/config/logic/getSession";
 import {
-	PACKAGE_STATUS,
-	SALE_STATUS,
-	activeStep,
-	carouselThumbnailsAtom,
-	packageBasicInfoAtom,
-} from '@/store/createPackgage.store';
-import { useMutation } from '@apollo/client';
-import { Button, FileButton, Group, Input, Title } from '@mantine/core';
-import { showNotification } from '@mantine/notifications';
-import { useAtom } from 'jotai';
-import Image from 'next/image';
-import { useRouter } from 'next/router';
-import React, { useState } from 'react';
-import { FaPlus } from 'react-icons/fa';
-import { FiUpload } from 'react-icons/fi';
-import { HiOutlinePhotograph } from 'react-icons/hi';
+  PACKAGE_STATUS,
+  SALE_STATUS,
+  activeStep,
+  carouselThumbnailsAtom,
+  packageBasicInfoAtom,
+} from "@/store/createPackgage.store";
+import { useMutation } from "@apollo/client";
+import { Button, FileButton, Group, Input, Title } from "@mantine/core";
+import { showNotification } from "@mantine/notifications";
+import { useAtom } from "jotai";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import React, { useState } from "react";
+import { FaPlus } from "react-icons/fa";
+import { FiUpload } from "react-icons/fi";
+import { HiOutlinePhotograph } from "react-icons/hi";
 
 const UploadThumbnails: React.FC = () => {
-	const { user } = useGetSession();
-	const { query } = useRouter();
-	const [uploading, setUploading] = useState<string>('');
-	const [, onChangeStep] = useAtom(activeStep);
-	const [packageBasicInfo, onChangePackageBasicInfo] =
-		useAtom(packageBasicInfoAtom);
-	const [thumbnail, setThumbnail] = useState<string>(
-		packageBasicInfo?.thumbnail!
-	);
-	const [submitType, setSubmitType] = useState('');
-	const [carouselImg, onChangeCarouselThumbnails] = useAtom(
-		carouselThumbnailsAtom
-	);
-	const [carouselThumbnails, setCarouselThumbnails] = useState<any>(
-		carouselImg || ['']
-	);
-	const nextStep = () =>
-		onChangeStep((currentStep) =>
-			currentStep < 3 ? currentStep + 1 : currentStep
-		);
+  const { user } = useGetSession();
+  const { query } = useRouter();
+  const [uploading, setUploading] = useState<string>("");
+  const [, onChangeStep] = useAtom(activeStep);
+  const [packageBasicInfo, onChangePackageBasicInfo] =
+    useAtom(packageBasicInfoAtom);
+  const [thumbnail, setThumbnail] = useState<string>(
+    packageBasicInfo?.thumbnail!
+  );
+  const [submitType, setSubmitType] = useState("");
+  const [carouselImg, onChangeCarouselThumbnails] = useAtom(
+    carouselThumbnailsAtom
+  );
+  const [carouselThumbnails, setCarouselThumbnails] = useState<any>(
+    carouselImg || [""]
+  );
+  const nextStep = () =>
+    onChangeStep((currentStep) =>
+      currentStep < 3 ? currentStep + 1 : currentStep
+    );
 
-	// upload thumbnail
-	const handleUploadPackageThumbnail = async (
-		file: File,
-		actionName: string,
-		onChangeThumbnail: any,
-		idx?: number
-	) => {
-		try {
-			setUploading(actionName);
-			const { file_upload_cloudinary } = fileUploader(
-				file,
-				'travel_package_thumbnails'
-			);
+  // upload thumbnail
+  const handleUploadPackageThumbnail = async (
+    file: File,
+    actionName: string,
+    onChangeThumbnail: any,
+    idx?: number
+  ) => {
+    try {
+      setUploading(actionName);
+      const { file_upload_cloudinary } = fileUploader(
+        file,
+        "travel_package_thumbnails"
+      );
 
-			const uploaded = await file_upload_cloudinary();
-			if (uploaded) {
-				showNotification({
-					title: 'File uploaded successfully!',
-					message: 'Save file to database.',
-					color: 'teal',
-				});
-				if (idx === 0 || idx! > 0) {
-					if (idx !== 0) {
-						onChangeThumbnail((prev: any) => {
-							prev.splice(idx, 1);
-							return [...prev, uploaded];
-						});
-					} else {
-						onChangeThumbnail((prev: any) => {
-							prev[idx] = uploaded;
-							return [...prev];
-						});
-					}
-				} else {
-					onChangeThumbnail(() => uploaded);
-				}
-				setUploading('');
-			} else {
-				setUploading('');
-			}
-		} catch (err: any) {
-			setUploading('');
-			showNotification({
-				title: 'Failed to upload file!',
-				message: err.message,
-				color: 'red',
-			});
-		}
-	};
+      const uploaded = await file_upload_cloudinary();
+      if (uploaded) {
+        showNotification({
+          title: "File uploaded successfully!",
+          message: "Save file to database.",
+          color: "teal",
+        });
+        if (idx === 0 || idx! > 0) {
+          if (idx !== 0) {
+            onChangeThumbnail((prev: any) => {
+              prev.splice(idx, 1);
+              return [...prev, uploaded];
+            });
+          } else {
+            onChangeThumbnail((prev: any) => {
+              prev[idx] = uploaded;
+              return [...prev];
+            });
+          }
+        } else {
+          onChangeThumbnail(() => uploaded);
+        }
+        setUploading("");
+      } else {
+        setUploading("");
+      }
+    } catch (err: any) {
+      setUploading("");
+      showNotification({
+        title: "Failed to upload file!",
+        message: err.message,
+        color: "red",
+      });
+    }
+  };
 
-	// create travel packages
-	const [createPackage, { loading: creatingPackage }] = useMutation(
-		CREATE_TRAVEL_PACKAGE,
-		Notify({
-			sucTitle: 'Package created successfully!',
-			sucMessage: 'Please refetch package list.',
-			errMessage: 'Failed to create package.',
-		})
-	);
+  // create travel packages
+  const [createPackage, { loading: creatingPackage }] = useMutation(
+    CREATE_TRAVEL_PACKAGE,
+    Notify({
+      sucTitle: "Package created successfully!",
+      sucMessage: "Please refetch package list.",
+      errMessage: "Failed to create package.",
+    })
+  );
 
-	// update package mutation
-	const [updatePackage, { loading: updatingPackage }] = useMutation(
-		Update_Tour_Package,
-		Notify({
-			sucTitle: 'Package saved successfully!',
-			errMessage: 'Failed to save package.',
-		})
-	);
+  // update package mutation
+  const [updatePackage, { loading: updatingPackage }] = useMutation(
+    Update_Tour_Package,
+    Notify({
+      sucTitle: "Package saved successfully!",
+      errMessage: "Failed to save package.",
+    })
+  );
 
-	//submit create package function
-	const onSubmit = () => {
-		onChangePackageBasicInfo({
-			...packageBasicInfo,
-			thumbnail: thumbnail,
-		});
-		onChangeCarouselThumbnails(carouselThumbnails);
+  //submit create package function
+  const onSubmit = () => {
+    onChangePackageBasicInfo({
+      ...packageBasicInfo,
+      thumbnail: thumbnail,
+    });
+    onChangeCarouselThumbnails(carouselThumbnails);
 
-		if (submitType === 'save') {
-			if (query?.packageId) {
-				updatePackage({
-					variables: {
-						input: {
-							...packageBasicInfo,
-							thumbnail: thumbnail,
-							carouselThumbnails,
-							saleStatus:
-								packageBasicInfo?.salePrice === 0
-									? SALE_STATUS.SALE
-									: SALE_STATUS.FIXED,
-							packageStatus: PACKAGE_STATUS.UPCOMING,
-							isPublished: false,
-							author: user?._id,
-							_id: query?.packageId,
-						},
-					},
-				});
-			} else {
-				createPackage({
-					variables: {
-						input: {
-							...packageBasicInfo,
-							thumbnail: thumbnail,
-							carouselThumbnails,
-							saleStatus:
-								packageBasicInfo?.salePrice === 0
-									? SALE_STATUS.SALE
-									: SALE_STATUS.FIXED,
-							packageStatus: PACKAGE_STATUS.UPCOMING,
-							isPublished: false,
-							author: user?._id,
-						},
-					},
-				});
-			}
-		} else {
-			nextStep();
-		}
-	};
+    if (submitType === "save") {
+      if (query?.packageId) {
+        updatePackage({
+          variables: {
+            input: {
+              ...packageBasicInfo,
+              thumbnail: thumbnail,
+              carouselThumbnails,
+              saleStatus:
+                packageBasicInfo?.salePrice === 0
+                  ? SALE_STATUS.SALE
+                  : SALE_STATUS.FIXED,
+              packageStatus: PACKAGE_STATUS.UPCOMING,
+              isPublished: false,
+              author: user?._id,
+              _id: query?.packageId,
+            },
+          },
+        });
+      } else {
+        createPackage({
+          variables: {
+            input: {
+              ...packageBasicInfo,
+              thumbnail: thumbnail,
+              carouselThumbnails,
+              saleStatus:
+                packageBasicInfo?.salePrice === 0
+                  ? SALE_STATUS.SALE
+                  : SALE_STATUS.FIXED,
+              packageStatus: PACKAGE_STATUS.UPCOMING,
+              isPublished: false,
+              author: user?._id,
+            },
+          },
+        });
+      }
+    } else {
+      nextStep();
+    }
+  };
 
-	return (
-		<div>
-			<div className='flex justify-center items-center gap-8 mt-5'>
-				<Input.Wrapper
-					label='Upload thumbnail'
-					size='md'
-					className='lg:w-5/12 w-full relative'
-				>
-					<div className='h-[200px] bg-[#212231] flex items-center justify-center'>
-						{thumbnail ? (
-							<Image
-								src={thumbnail}
-								alt='Thumbnail'
-								width={300}
-								className='!w-full'
-								height={200}
-							/>
-						) : (
-							<HiOutlinePhotograph size={50} color='#5F3DC4' />
-						)}
-					</div>
-					<div className='absolute bottom-3 right-3'>
-						<FileButton
-							onChange={(file: File) =>
-								handleUploadPackageThumbnail(
-									file,
-									'PACKAGE_THUMBNAIL',
-									setThumbnail
-								)
-							}
-							accept='image/png,image/jpeg'
-						>
-							{(props) => (
-								<Button
-									loading={uploading === 'PACKAGE_THUMBNAIL'}
-									color='violet'
-									{...props}
-								>
-									<FiUpload color='#d0d1db' size={16} />
-								</Button>
-							)}
-						</FileButton>
-					</div>
-				</Input.Wrapper>
-			</div>
+  return (
+    <div>
+      <div className="flex justify-center items-center gap-8 mt-5">
+        <Input.Wrapper
+          label="Upload thumbnail (Resoulation:265/208, Size: below 3mb)"
+          size="md"
+          className="lg:w-5/12 w-full relative"
+        >
+          <div className="h-[200px] bg-[#212231] flex items-center justify-center">
+            {thumbnail ? (
+              <Image
+                src={thumbnail}
+                alt="Thumbnail"
+                width={300}
+                className="!w-full"
+                height={200}
+              />
+            ) : (
+              <HiOutlinePhotograph size={50} color="#5F3DC4" />
+            )}
+          </div>
+          <div className="absolute bottom-3 right-3">
+            <FileButton
+              onChange={(file: File) =>
+                handleUploadPackageThumbnail(
+                  file,
+                  "PACKAGE_THUMBNAIL",
+                  setThumbnail
+                )
+              }
+              accept="image/png,image/jpeg"
+            >
+              {(props) => (
+                <Button
+                  loading={uploading === "PACKAGE_THUMBNAIL"}
+                  color="violet"
+                  {...props}
+                >
+                  <FiUpload color="#d0d1db" size={16} />
+                </Button>
+              )}
+            </FileButton>
+          </div>
+        </Input.Wrapper>
+      </div>
 
-			<div className='mt-16'>
-				<div className='lg:flex items-center justify-between '>
-					<Title
-						order={3}
-						fz={{ lg: 22, sm: 18 }}
-						ff={'Nunito sans, sans-serif'}
-						className='lg:mb-0 mb-2'
-					>
-						Package carousel thumbnails
-					</Title>
-					<Button
-						leftIcon={<FaPlus size={16} />}
-						color='violet'
-						size='md'
-						ff={'Nunito sans, sans-serif'}
-						variant='light'
-						onClick={() =>
-							setCarouselThumbnails((thumbnails: string[]) => [
-								...thumbnails,
-								'',
-							])
-						}
-					>
-						Add new file
-					</Button>
-				</div>
-				<div className='grid lg:grid-cols-2 gap-5 mt-10'>
-					{carouselThumbnails?.map((thumbnail: string, idx: number) => (
-						<Input.Wrapper
-							key={idx}
-							label={`Upload carousel thumbnail ${idx + 1} (size 770/300 px)`}
-							size='md'
-							className='relative'
-						>
-							<div className='h-[250px] bg-[#212231] flex items-center justify-center'>
-								{thumbnail ? (
-									<Image
-										src={thumbnail}
-										alt='Thumbnail'
-										width={200}
-										className='!w-full'
-										height={250}
-									/>
-								) : (
-									<HiOutlinePhotograph color='#5F3DC4' size={50} />
-								)}
-							</div>
-							<div className='absolute bottom-3 right-3 gap-1 flex items-center'>
-								<Button
-									color='red'
-									onClick={() => {
-										setCarouselThumbnails((prev: string[]) => {
-											prev.splice(idx, 1);
-											return [...prev];
-										});
-									}}
-								>
-									Remove
-								</Button>
-								<FileButton
-									onChange={(file: File) =>
-										handleUploadPackageThumbnail(
-											file,
-											`PACKAGE_CAROUSEL_THUMBNAILS_${idx}`,
-											setCarouselThumbnails,
-											idx
-										)
-									}
-									accept='image/png,image/jpeg'
-								>
-									{(props) => (
-										<Button
-											loading={
-												uploading === `PACKAGE_CAROUSEL_THUMBNAILS_${idx}`
-											}
-											color='violet'
-											{...props}
-										>
-											<FiUpload size={16} />
-										</Button>
-									)}
-								</FileButton>
-							</div>
-						</Input.Wrapper>
-					))}
-				</div>
-			</div>
+      <div className="mt-16">
+        <div className="lg:flex items-center justify-between ">
+          <Title
+            order={3}
+            fz={{ lg: 22, sm: 18 }}
+            ff={"Nunito sans, sans-serif"}
+            className="lg:mb-0 mb-2"
+          >
+            Package carousel thumbnails
+          </Title>
+          <Button
+            leftIcon={<FaPlus size={16} />}
+            color="violet"
+            size="md"
+            ff={"Nunito sans, sans-serif"}
+            variant="light"
+            onClick={() =>
+              setCarouselThumbnails((thumbnails: string[]) => [
+                ...thumbnails,
+                "",
+              ])
+            }
+          >
+            Add new file
+          </Button>
+        </div>
+        <div className="grid lg:grid-cols-2 gap-5 mt-10">
+          {carouselThumbnails?.map((thumbnail: string, idx: number) => (
+            <Input.Wrapper
+              key={idx}
+              label={`Upload carousel thumbnail ${idx + 1} (size 770/300 px)`}
+              size="md"
+              className="relative"
+            >
+              <div className="h-[250px] bg-[#212231] flex items-center justify-center">
+                {thumbnail ? (
+                  <Image
+                    src={thumbnail}
+                    alt="Thumbnail"
+                    width={200}
+                    className="!w-full"
+                    height={250}
+                  />
+                ) : (
+                  <HiOutlinePhotograph color="#5F3DC4" size={50} />
+                )}
+              </div>
+              <div className="absolute bottom-3 right-3 gap-1 flex items-center">
+                <Button
+                  color="red"
+                  onClick={() => {
+                    setCarouselThumbnails((prev: string[]) => {
+                      prev.splice(idx, 1);
+                      return [...prev];
+                    });
+                  }}
+                >
+                  Remove
+                </Button>
+                <FileButton
+                  onChange={(file: File) =>
+                    handleUploadPackageThumbnail(
+                      file,
+                      `PACKAGE_CAROUSEL_THUMBNAILS_${idx}`,
+                      setCarouselThumbnails,
+                      idx
+                    )
+                  }
+                  accept="image/png,image/jpeg"
+                >
+                  {(props) => (
+                    <Button
+                      loading={
+                        uploading === `PACKAGE_CAROUSEL_THUMBNAILS_${idx}`
+                      }
+                      color="violet"
+                      {...props}
+                    >
+                      <FiUpload size={16} />
+                    </Button>
+                  )}
+                </FileButton>
+              </div>
+            </Input.Wrapper>
+          ))}
+        </div>
+      </div>
 
-			<Group position='right' mt={65}>
-				<Button
-					color='teal'
-					loading={creatingPackage || updatingPackage}
-					onClick={() => {
-						onSubmit();
-						setSubmitType('save');
-					}}
-				>
-					Save
-				</Button>
-				<Button
-					color='violet'
-					onClick={() => {
-						setSubmitType('next');
-						onSubmit();
-					}}
-				>
-					Next step
-				</Button>
-			</Group>
-		</div>
-	);
+      <Group position="right" mt={65}>
+        <Button
+          color="teal"
+          loading={creatingPackage || updatingPackage}
+          onClick={() => {
+            onSubmit();
+            setSubmitType("save");
+          }}
+        >
+          Save
+        </Button>
+        <Button
+          color="violet"
+          onClick={() => {
+            setSubmitType("next");
+            onSubmit();
+          }}
+        >
+          Next step
+        </Button>
+      </Group>
+    </div>
+  );
 };
 
 export default UploadThumbnails;
